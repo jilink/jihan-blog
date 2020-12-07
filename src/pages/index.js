20e245e78c82bddc3ddb6d2b14b4f57a93a28d7a
jilink <jimmyfac1@gmail.com>
2020-12-07 18:04:59 +0100

    Getting list of recettes on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,47 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO />
-    <h1>Jihan Blog</h1>
-    <p>Bienvenue sur le blog de Jihan</p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Accueil" />
+      <h1>Jihan Blog</h1>
+      <p>Bienvenue sur le blog de Jihan</p>
+      <ul>
+        {data.allMarkdownRemark.edges.map(({ node }) => {
+          return (
+            <Link key={node.id} to={`/recette${node.fields.slug}`}>
+              {" "}
+              <li>{node.frontmatter.title}</li>
+            </Link>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
 
+export const IndexPageQuery = graphql`
+  query IndexPageQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          frontmatter {
+            title
+            date
+            description
+            ingredients
+            path
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
