335684a7edecf936a661720006024f6bf0a231cb
jilink <jimmyfac1@gmail.com>
2020-12-12 10:27:37 +0100

    PageList & PageItem

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,28 +2,42 @@ import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 
+const PageItem = ({ id, title, fixedImg, slug, date, description }) => {
+  return (
+    <li className="list-item" key={id}>
+      <Link to={`/recette${slug}`}>
+        <div className="container">
+          <Img
+            title={title}
+            alt="Image de la recette indisponible"
+            fixed={fixedImg}
+          />
+          <span className="title">{title}</span>
+        </div>
+      </Link>
+      <figcaption>
+        <small>{date}</small>
+      </figcaption>
+      <p>{description}</p>
+      <Link to={`/recette${slug}`}>Découvrir la recette</Link>
+    </li>
+  )
+}
+
 const PageList = ({ list }) => {
   return (
     <ul className="list-pages">
       {list.map(({ node }) => {
         return (
-          <li className="list-item" key={node.id}>
-            <figure>
-              <Link to={`/recette${node.fields.slug}`}>
-                <Img
-                  title={node.frontmatter.title}
-                  alt="Image de la recette indisponible"
-                  fixed={node.frontmatter.image.childImageSharp.fixed}
-                />
-              </Link>
-              <figcaption>
-                <small>{node.frontmatter.date}</small>
-              </figcaption>
-            </figure>
-            <h5 className="title">{node.frontmatter.title}</h5>
-            <p>{node.frontmatter.description}</p>
-            <Link to={`/recette${node.fields.slug}`}>Découvrir la recette</Link>
-          </li>
+          <PageItem
+            key={node.id}
+            id={node.id}
+            title={node.frontmatter.title}
+            fixedImg={node.frontmatter.image.childImageSharp.fixed}
+            slug={node.fields.slug}
+            date={node.frontmatter.date}
+            description={node.frontmatter.description}
+          />
         )
       })}
     </ul>
